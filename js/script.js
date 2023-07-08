111ad41bd5e8d9c3d0750f407c16dfc0e0ed6b39
Adilson Corrêa <dihcorrea96@gmail.com>
2023-07-08 11:46:40 -0300

    Novo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-var termosECondicoes = `
-<h1>Termos e Condições</h1>
-<p>Este é um exemplo de termos e condições.</p>
-<p>...</p>
-`;
 
-var termosDiv = document.getElementById('termos');
-termosDiv.innerHTML = termosECondicoes;
-
+function imprimirPagina() {
+    window.print()
+}
