8fbcdc825d3a8266573ac122f4a65a04b10f7b10
Adilson Corrêa <dihcorrea96@gmail.com>
2023-07-08 11:16:24 -0300

    Coiso do Amor

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var termosECondicoes = `
+<h1>Termos e Condições</h1>
+<p>Este é um exemplo de termos e condições.</p>
+<p>...</p>
+`;
+
+var termosDiv = document.getElementById('termos');
+termosDiv.innerHTML = termosECondicoes;
+
